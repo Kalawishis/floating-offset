0c2d8c18e4cd5d7f83a5f6180c002e9d16dc01f1
andrewraytan <andrewraytan@gmail.com>
2018-05-11 20:57:33 -0500

    added multiplication function in normalize.py

--- normalize.py
+++ normalize.py
@@ -1,3 +1,14 @@
+def lcm(x, y):
+   if x > y:
+       z = x
+   else:
+       z = y
+   while(True):
+       if((z % x == 0) and (z % y == 0)):
+           lcm = z
+           break
+       z += 1
+   return lcm
 def norm(A,B,C):
     if C==1:
         A=A*B
@@ -36,4 +47,30 @@ def norm(A,B,C):
         A=A*coeff
     print(str(A) + " * (" + str(B) + ")^1/" + str(C))
 
-norm(1,6,2)
+
+def multiply(A1,B1,C1,A2,B2,C2):
+    if C1==C2:
+        A=A1*A2
+        B=B1*B2
+        C=C1
+        norm(A,B,C)
+    if (C1<0 and C2<0) or (C1>0 and C2>0):
+        is_C_neg = False
+        if C1<0:
+            is_C_neg = True
+            C1=abs(C1)
+            C2=abs(C2)
+        l = lcm(C1,C2)
+        B1 = B1 ** (l//C1)
+        B2 = B2 ** (l//C2)
+        C=l
+        B=B1*B2
+        A=A1*A2
+        if is_C_neg:
+            C=-C
+        norm(A,B,C)
+    else:
+        print("cannot multiply cleanly (there are fractions in form of radicals)")
+
+
+multiply(1,5,3,1,2,2)
